70669cf3b860b8a5aafa33fb00de1cd6cc6662d6
logikfabrik <anton@logikfabrik.se>
2020-07-19 20:33:52 +0200

    Makes changes to the time pickers

--- src/app/src/main/java/se/logikfabrik/hiit/widgets/MyTimePartPicker.kt
+++ src/app/src/main/java/se/logikfabrik/hiit/widgets/MyTimePartPicker.kt
@@ -10,24 +10,33 @@ open class MyTimePartPicker : NumberPicker {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        processXmlAttributes(attrs)
+        setXmlAttributes(attrs)
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        processXmlAttributes(attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        setXmlAttributes(attrs, defStyleAttr)
     }
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes) {
-        processXmlAttributes(attrs, defStyleAttr, defStyleRes)
-    }
-
-    private fun processXmlAttributes(attrs: AttributeSet, defStyleAttr: Int = 0, defStyleRes: Int = 0) {
-        val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.MyTimePartPicker, defStyleAttr, defStyleRes)
+    private fun setXmlAttributes(
+        attrs: AttributeSet,
+        defStyleAttr: Int = 0,
+        defStyleRes: Int = 0
+    ) {
+        val attributes = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.time_part_picker,
+            defStyleAttr,
+            defStyleRes
+        )
 
         try {
-            minValue = maxOf(attributes.getInt(R.styleable.MyTimePartPicker_minValue, 0), 0)
-            maxValue = maxOf(attributes.getInt(R.styleable.MyTimePartPicker_maxValue, 60), 0)
-            value = maxOf(attributes.getInt(R.styleable.MyTimePartPicker_defaultValue, 0), 0)
+            minValue = maxOf(attributes.getInt(R.styleable.time_part_picker_minValue, 0), 0)
+            maxValue = maxOf(attributes.getInt(R.styleable.time_part_picker_maxValue, 60), 0)
+            value = maxOf(attributes.getInt(R.styleable.time_part_picker_defaultValue, 0), 0)
         } finally {
             attributes.recycle()
         }
@@ -36,13 +45,15 @@ open class MyTimePartPicker : NumberPicker {
     override fun onFinishInflate() {
         super.onFinishInflate()
 
-        if(minValue >= maxValue) {
+        if (minValue >= maxValue) {
+            displayedValues = arrayOf<String>()
+
             return
         }
 
         val range = IntRange(minValue, maxValue)
 
-        val values = range.map { value -> if(value < 10) "0$value" else "$value"; }.toTypedArray()
+        val values = range.map { value -> if (value < 10) "0$value" else "$value"; }.toTypedArray()
 
         displayedValues = values
     }
